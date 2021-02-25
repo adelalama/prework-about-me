acacbd108ac6fe727a844aec91da2ef9739827bc
Alejandro De La Lama <delalama.7@gmail.com>
2021-02-24 21:53:02 -0600

    Adding modules 3 through 8

---
+++ Module6/javascript.js
@@ -0,0 +1,23 @@
+
+function grow(){
+    document.getElementById("box").style.height = "175px";
+    document.getElementById("box").style.width = "175px";
+}
+
+function blue(){
+    document.getElementById("box").style.backgroundColor = "Blue";
+}
+
+function fade(){
+    document.getElementById("box").style.opacity = "0.1";
+}
+
+function reset(){
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.width = "150px";
+    document.getElementById("box").style.backgroundColor = "Orange";
+    document.getElementById("box").style.opacity = "1"
+}
+
+
+
